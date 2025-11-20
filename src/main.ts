ef54e578a8e64a4fa559d72a1d89782257da5ce2
Hasan <hasan.sheikh689@gmail.com>
2025-11-20 22:07:29 +0500

    A basic frontend with just Hello World, V1
    Signed-off-by: Hasan <hasan.sheikh689@gmail.com>

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1 class="text-3xl font-bold underline">
+      Hello, World!
+    </h1>
+  </div>
+`
+
